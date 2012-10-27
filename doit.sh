4fc6468e5028969db1d4baffef4b55da14aee7b2
otfallen <f.fallen45@gmail.com>
2012-10-27 04:54:02 +0000

    doit.sh: figure out cores through /proc/cpuinfo

--- doit.sh
+++ doit.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-make clean && make -j3
+CORES=`grep processor /proc/cpuinfo | wc -l`
+MAKEOPT=$(($CORES + 1))
+
+make clean && make -j${MAKEOPT} || echo "errors occured!"; exit
 cd bin && ./a && cd ..
+
