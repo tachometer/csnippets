52656293fc9e1a0319f63930464f4f3ef6865b26
otfallen <f.fallen45@gmail.com>
2012-11-14 13:38:18 +0000

    Fix crash in stack.c

--- examples/stack.c
+++ examples/stack.c
@@ -17,9 +17,9 @@ int main(int argc, char **argv)
 
     stack_init(&on_stack, 5);
     for (i = 0; i < argc; i++)
-        stack_push(&on_stack, argv[i], -1, NULL);
+        stack_push(&on_stack, strdup(argv[i]), -1, NULL);
 
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < argc; i++) {
         printf("on_stack.ptr[%d]: %s\n",i, (char *)on_stack.ptr[i]);
         stack_remove(&on_stack, on_stack.ptr[i], stack_compare_string, NULL,
             true);
